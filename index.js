d0865feec15e41c0083bcc91cbdcc628a9d45686
Pontoxx <ivangiulkovich@hotmail.com>
2020-06-26 18:36:22 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const port= process.env.PORT || 3000;
 const express = require ("express");
 const app = express ();
 
+app.use(express.static("public"));
 app.get ("/", (req,res) => {
     res.send ("Hello world");
 });
